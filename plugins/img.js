de64c4c009d6fb14de4f94c76697ea338d5fcc46
GOD ZENITSU <164893015+godzenitsu@users.noreply.github.com>
2024-06-30 13:12:56 +0530

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -1,11 +1,11 @@
 const config = require('../config')
-const { nezuko, commands } = require('../command')
+const { cmd, commands } = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')	
 
 
 
 
-nezuko({
+cmd({
     pattern: "img",
     react: '👾',
     desc: 'to down images',
@@ -13,10 +13,10 @@ nezuko({
     use: '.img dark',
     filename: __filename
 },
-async(conn, mek, m,{from, l, prefix, quoted, body, isNezuko, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
     
-    if (!q) throw `Example: ${prefix + command} Zenitsu`
+    if (!q) throw `Example: ${prefix + command} Bike`
 
     let gis = require('g-i-s')
     gis(q, async (error, result) => {
@@ -31,10 +31,10 @@ try{
         for (let i = 0; i < topImages.length; i++) {
             const imageUrl = topImages[i].url
           let Message = {
-              image: { url: imageUrl },caption: `*-------「 IMAGE SEARCH 」-------*\n⭐*Image ${i + 1}`,
+              image: { url: imageUrl },caption: `*-------「 IMAGE SEARCH 」-------*\n🤠*Image ${i + 1}`,
            }
 
-//let senda = await conn.sendMessage(from, { document: {url: imageUrl },fileName: 'image' + '.jpg', mimetype: 'image/jpeg' ,caption: `*-------「 NEZUKO MD IMAGE SEARCH 」-------*\n⭐ *Query* : ${q}\n\n🔗 *Image ${i + 1} Url* : ${imageUrl}`,}, { quoted: mek })  
+//let senda = await conn.sendMessage(from, { document: {url: imageUrl },fileName: 'image' + '.jpg', mimetype: 'image/jpeg' ,caption: `*-------「 VAJIRA MD GIMAGE SEARCH 」-------*\n🤠 *Query* : ${q}\n\n🔗 *Image ${i + 1} Url* : ${imageUrl}`,}, { quoted: mek })  
 		
             conn.sendMessage(from, Message, { quoted: mek })
         }
